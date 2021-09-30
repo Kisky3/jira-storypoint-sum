ce060a700c0434022c9ce6297672a4d5980b6bad
kisky3 <kisky3@126.com>
2021-09-30 21:49:59 +0900

    :+1: add a part of URL

--- api/info.ts
+++ api/info.ts
@@ -2,6 +2,7 @@ import axios, { AxiosPromise } from "axios";
 
 export default {
   getJiraStoryPointData(): AxiosPromise | void {
+    let URL = process.env.JIRA_API_BASE_URL;
     return axios.post("", []).then((res) => res.data);
   },
 };
