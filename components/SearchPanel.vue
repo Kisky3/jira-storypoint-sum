d5f53248d239bf4d325cfe76d246811c970c7d9d
kisky3 <kisky3@126.com>
2021-09-30 22:33:03 +0900

    :+1: add searchpanel molecules

---
+++ components/SearchPanel.vue
@@ -0,0 +1,17 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <!-- Search Panel Start -->
+
+    <!-- Search Panel End -->
+  </div>
+</template>
+<style scope lang="scss"></style>
+
+<script>
+export default {
+  components: {
+    ,
+  },
+}
+</script>
