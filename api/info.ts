05ed59beeb0a44bbd17b54c755440a5097dc0b86
kisky3 <kisky3@126.com>
2021-09-30 21:43:19 +0900

    :+1: remove link

--- api/info.ts
+++ api/info.ts
@@ -2,8 +2,6 @@ import axios, { AxiosPromise } from "axios";
 
 export default {
   getJiraStoryPointData(): AxiosPromise | void {
-    return axios
-      .post("https://marketenterprise.atlassian.net/rest/api/2/search?jql=", [])
-      .then((res) => res.data);
+    return axios.post("", []).then((res) => res.data);
   },
 };
