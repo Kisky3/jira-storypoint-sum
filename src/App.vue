4072b5023b375fcce9c0ed5c6914781efd9cbc05
XiangYu <kisky3@126.com>
2021-12-04 19:30:20 +0900

    :+1: add basic component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <Header />
+    <Container />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Vue from 'vue'
+import Container from './components/organisms/Container.vue'
+import Header from './components/molecules/Header.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default Vue.extend({
+  components: { Container, Header },
+})
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
