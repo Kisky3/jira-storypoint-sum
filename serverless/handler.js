cd635f481dad2964d580f1b8d61eaeea10416324
XiangYu <kisky3@126.com>
2021-12-16 20:12:49 +0900

    :+1: update serveless and install vue element ui

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,18 +1,16 @@
+// handler.js
+
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
+module.exports.hello = function (event, context, callback) {
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Test success!!!!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+    },
+    body: JSON.stringify({ message: 'Hello World!' }),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  callback(null, response);
 };
