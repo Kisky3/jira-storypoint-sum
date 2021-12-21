3285a1784ffc1810b08c228ea2eff155bff50453
XiangYu <kisky3@126.com>
2021-12-21 21:00:12 +0900

    :+1: mock data and build express server

---
+++ mock/index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const adminLogin = require('./data/admin_login.json');
+const vipLogin = require('./data/vip_login.json');
+const url = require('url');
+
+app.get("/login", (res, req) => {
+    const userType = url.parse(req.url, true).query.user;
+    switch (userType) {
+        case 'vip': 
+            res.send(vipLogin);
+            break
+        case 'admin':
+            res.send(adminLogin);
+            break
+    }
+})
+app.listen(3300, ()=> {
+    console.log("run serve at 3300");
+})
